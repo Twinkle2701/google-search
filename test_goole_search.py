05d05ad9ba3aeae187360403337c386281a5de5c
Twinkle2701 <twinklepatel2701@gmail.com>
2026-04-27 23:40:33 +0530

    Update test_goole_search.py

--- test_goole_search.py
+++ test_goole_search.py
@@ -2,14 +2,14 @@ from playwright.sync_api import sync_playwright
 
 def test_google_search():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
+        browser = p.chromium.launch(headless=False) #Luanch the browser
+        page = browser.new_page() #Creates the new tab
 
-        page.goto("https://www.google.com")
-        page.fill("#APjFqb", "Twinkle")
-        page.press("#APjFqb", "Enter")
+        page.goto("https://www.google.com") #Navigate to the URL
+        page.fill("#APjFqb", "Twinkle") #Enter the test into the search box
+        page.press("#APjFqb", "Enter") #Pressing the Enter
 
-        page.wait_for_selector("h3")
-        page.locator("a:has(h3)").first.click()
+        page.wait_for_selector("h3") #Wait for the element to load
+        page.locator("a:has(h3)").first.click() #Click on the First result
 
-        browser.close()
+        browser.close() #Close the broswer
